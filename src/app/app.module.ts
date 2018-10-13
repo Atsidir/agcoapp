a67612b11d1af080ba6261484a3e3bcaac1fa8a6
503005052 <tamas.torjai@ge.com>
2018-10-13 11:06:19 +0200

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from  '@angular/common/http';
+import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { TrackermapComponent } from './trackermap/trackermap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackermapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
